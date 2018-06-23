143c8874f0591959c56b9e7e8bb92321926755c8
Ashley Stallings Rhodes <aesdev421@gmail.com>
2018-06-23 13:04:47 -0500

    count up timer and hard coded sprint counter

--- app/index.js
+++ app/index.js
@@ -7,39 +7,42 @@ import clock from "clock";
 let background = document.getElementById("background");
 let play = document.getElementById("playButton");
 let pause = document.getElementById("pauseButton");
+let progressArc = document.getElementById("progressArc");
+let countdown = document.getElementById("countdown");
+let sprintCounter = document.getElementById("sprintCounter");
 
 let currentIndex = background.value;
 background.value = 0;
 
 clock.granularity = "seconds";
-let startTime;
-let hours = 0;
-let minutes = 0;
+let intervalInSeconds = 1500;
+let formattedHours = 0;
+let formattedMinutes = 0;
+let formattedSeconds = 0;
 let seconds = 0;
 
-function minutesToAngle(minutes){
-  return (360/60) * minutes;
-}
-
 function secondsToAngle(seconds){
-  return (360/60) * seconds;
+  return (360/intervalInSeconds) * seconds;
 }
 
 function progress(){
   seconds += 1;
-  if (seconds >= 60){
-    minutes += 1;
-    seconds = 0;
-    if (minutes >= 60){
-      hours += 1;
-      minutes = 0;
+  formattedSeconds++;
+  if (formattedSeconds >= 60){
+    formattedMinutes += 1;
+    formattedSeconds = 0;
+    if (formattedMinutes >= 60){
+      formattedHours += 1;
+      formattedMinutes = 0;
     }
   }
   
-  console.log(hours + ":" + minutes + ":" + seconds);
+  let a = secondsToAngle(seconds);
+  progressArc.sweepAngle = a;
+  countdown.text = formattedHours + ":" + formattedMinutes + ":" + formattedSeconds;
   
 }
 
 clock.ontick = () => progress();
 
-play.onactivate = (evt) => startTime = new Date();
+play.onactivate = (evt) => {}
