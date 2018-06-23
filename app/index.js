d33f4180abe7fcf4e8a68411b2b53b46a340da0c
Ashley Stallings Rhodes <aesdev421@gmail.com>
2018-06-22 19:04:09 -0500

    outline for gui

---
+++ app/index.js
@@ -0,0 +1,10 @@
+/*
+ * Entry point for the watch app
+ */
+import document from "document";
+
+let background = document.getElementById("background");
+
+let currentIndex = background.value;
+
+background.value = 0;
