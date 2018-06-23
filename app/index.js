25559940372cc6f02bb0f79f28175b3d1546ab7a
Ashley Stallings Rhodes <aesdev421@gmail.com>
2018-06-22 23:37:54 -0500

    clock ticking up by seconds

--- app/index.js
+++ app/index.js
@@ -2,9 +2,44 @@
  * Entry point for the watch app
  */
 import document from "document";
+import clock from "clock";
 
 let background = document.getElementById("background");
+let play = document.getElementById("playButton");
+let pause = document.getElementById("pauseButton");
 
 let currentIndex = background.value;
 background.value = 0;
 
+clock.granularity = "seconds";
+let startTime;
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+
+function minutesToAngle(minutes){
+  return (360/60) * minutes;
+}
+
+function secondsToAngle(seconds){
+  return (360/60) * seconds;
+}
+
+function progress(){
+  seconds += 1;
+  if (seconds >= 60){
+    minutes += 1;
+    seconds = 0;
+    if (minutes >= 60){
+      hours += 1;
+      minutes = 0;
+    }
+  }
+  
+  console.log(hours + ":" + minutes + ":" + seconds);
+  
+}
+
+clock.ontick = () => progress();
+
+play.onactivate = (evt) => startTime = new Date();
